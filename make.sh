0f6d8cc4dc29d5d3981446570ef7598426a44db4
Roy Lee <25062566@qq.com>
2025-05-06 07:45:45 +0000

    test

--- make.sh
+++ make.sh
@@ -1,1 +1,5 @@
-./compile.sh BOARD=xl-wzb211 RELEASE=noble BRANCH=current BUILD_DESKTOP=no BUILD_MINIMAL=yes BUILD_ONLY=u-boot KERNEL_CONFIGURE=no 
+#!/bin/bash 
+
+export http_proxy="http://localhost:3218"
+export https_proxy="http://localhost:3218"
+./compile.sh BOARD="xl-wzb211" RELEASE=noble BRANCH=current BUILD_DESKTOP=no BUILD_MINIMAL=yes BUILD_ONLY=u-boot KERNEL_CONFIGURE=no
