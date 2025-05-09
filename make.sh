00e65a5ef29043cfffa9b88460900a6ce77a0c18
Roy Lee <25062566@qq.com>
2025-05-09 05:53:19 +0000

    patch u-boot for xl-wzb211

--- make.sh
+++ make.sh
@@ -1,5 +1,2 @@
-#!/bin/bash 
-
-export http_proxy="http://localhost:3218"
-export https_proxy="http://localhost:3218"
-./compile.sh BOARD="xl-wzb211" RELEASE=noble BRANCH=current BUILD_DESKTOP=no BUILD_MINIMAL=yes BUILD_ONLY=u-boot KERNEL_CONFIGURE=no
+#!/bin/bash
+./compile.sh BOARD="xl-wzb211" RELEASE=noble BRANCH=current BUILD_DESKTOP=no BUILD_MINIMAL=yes KERNEL_CONFIGURE=no
